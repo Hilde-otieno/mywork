14014579be0b455615dcd56916044a0450a2c184
Hilde-Otieno <meghanadhiambootieno@gmail.com>
2024-04-14 13:45:30 +0300

    add file

---
+++ tuples.py
@@ -0,0 +1,31 @@
+names= ('Megan', 'Hilde', 'Greg', 'Otieno', 'Adhiambo')
+print(names)
+
+numbers= (5, 19, 12, 8, 2)
+print(numbers)
+
+word= ('My name is Alva')
+print(word)
+
+combine= names+numbers
+print(combine)
+name= list(names)
+print(name)
+name.insert(2,'Julius')
+print(name)
+newword= tuple(name)
+print(newword)
+name = list(names)
+print(name)
+name.remove('Greg')
+print(name)
+name1= tuple(name)
+print(name1)
+print(names[1])
+print(numbers[-2])
+print(combine[2:4])
+print(combine[1:4])
+
+
+
+
